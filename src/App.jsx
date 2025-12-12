8a4b4887231060e66d4235c4160e8041d243fbee
Healthy Lifestyle <jennalwill@gmail.com>
2025-12-12 10:30:41 -0800

    Create App component with basic layout

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 28, fontWeight: 700 }}>Iterra-GO</h1>
+      <p style={{ marginTop: 12 }}>
+        If you can read this, Base44 is no longer being served.
+      </p>
+    </div>
+  );
+}
