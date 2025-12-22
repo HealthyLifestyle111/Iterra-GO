491929eab94c09b781e6cb483bde9d7a319fb40b
Healthy Lifestyle <jennalwill@gmail.com>
2025-12-22 20:10:41 +0000

    Wrap app in global ErrorBoundary to surface runtime errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
 import FirstRunSetup from "@/components/FirstRunSetup"
+import ErrorBoundary from "@/components/ErrorBoundary"
+
 
 function App() {
   return (
-    <>
+    <ErrorBoundary>
       <FirstRunSetup />
       <Pages />
       <Toaster />
-    </>
+    </ErrorBoundary>
   )
 }
 
