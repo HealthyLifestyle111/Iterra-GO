b745f5a420e4ff62ab889324ad1d8b53199e8684
Healthy Lifestyle <jennalwill@gmail.com>
2026-03-18 15:01:28 -0700

    Refactor index.jsx to use App component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,93 +1,10 @@
-import Layout from "./Layout.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import Home from "./Home";
-
-import WellnessIntake from "./WellnessIntake";
-
-import ServiceDetail from "./ServiceDetail";
-
-import HomeEssentials from "./HomeEssentials";
-
-import LeadershipWisdom from "./LeadershipWisdom";
-
-import BackOffice from "./BackOffice";
-
-import WellnessIntakeResult from "./WellnessIntakeResult";
-
-import SpecializedIntake from "./SpecializedIntake";
-
-import LotusAI from "../components/LotusAI";
-
-import { Route, Routes, useLocation } from 'react-router-dom';
-
-const PAGES = {
-    
-    Home: Home,
-    
-    WellnessIntake: WellnessIntake,
-    
-    ServiceDetail: ServiceDetail,
-    
-    HomeEssentials: HomeEssentials,
-    
-    LeadershipWisdom: LeadershipWisdom,
-    
-    BackOffice: BackOffice,
-    
-    WellnessIntakeResult: WellnessIntakeResult,
-    
-    SpecializedIntake: SpecializedIntake,
-    
-}
-
-function _getCurrentPage(url) {
-    if (url.endsWith('/')) {
-        url = url.slice(0, -1);
-    }
-    let urlLastPart = url.split('/').pop();
-    if (urlLastPart.includes('?')) {
-        urlLastPart = urlLastPart.split('?')[0];
-    }
-
-    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
-    return pageName || Object.keys(PAGES)[0];
-}
-
-// Create a wrapper component that uses useLocation inside the Router context
-function PagesContent() {
-    const location = useLocation();
-    const currentPage = _getCurrentPage(location.pathname);
-    
-    return (
-        <Layout currentPageName={currentPage}>
-            <Routes>            
-                
-                    <Route path="/" element={<Home />} />
-                
-                
-                <Route path="/Home" element={<Home />} />
-                
-                <Route path="/WellnessIntake" element={<WellnessIntake />} />
-                
-                <Route path="/ServiceDetail" element={<ServiceDetail />} />
-                
-                <Route path="/HomeEssentials" element={<HomeEssentials />} />
-                
-                <Route path="/LeadershipWisdom" element={<LeadershipWisdom />} />
-                
-                <Route path="/BackOffice" element={<BackOffice />} />
-                
-                <Route path="/WellnessIntakeResult" element={<WellnessIntakeResult />} />
-                
-                <Route path="/SpecializedIntake" element={<SpecializedIntake />} />
-                
-                <Route path="/ai" element={<LotusAI />} />
-                
-            </Routes>
-        </Layout>
-    );
-}
-
-export default function Pages() {
-    return <PagesContent />;
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
